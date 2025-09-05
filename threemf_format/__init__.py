e21f6d6007bb508bd81631a5633b6f256631568e
Hell of the Devil <hell_of_the_devil@hotmail.co.uk>
2025-09-05 03:06:48 +0100

    Refractored project to be more streamline, Added material support (colors only for now)

--- threemf_format/__init__.py
+++ threemf_format/__init__.py
@@ -1,65 +1,78 @@
 import bpy, bmesh, os, importlib, sys, subprocess
 from bpy.props import StringProperty, BoolProperty
-from bpy.types import Context
+from bpy.types import Context, Material
 from bpy_extras.io_utils import ImportHelper, ExportHelper
 import lib3mf
-## Import Operator
 
-class Import3MF(bpy.types.Operator, ImportHelper):
-    """
-        Import a 3mf file
-    """
-    bl_idname = "import_3mf.3mf"
-    bl_label = "Import 3MF (3D Manufacturing Format)"
-    bl_options = {'PRESET', 'UNDO'}
+from .utils import lib3mf_color_to_linear, create_principled_material, create_texture_material
 
-    filename_ext = ".3mf"
-    filter_glob: StringProperty(default="*.3mf", options={'HIDDEN'}, maxlen=255) ## type: ignore
-    
-    def execute(self, context):
-        filepath = self.filepath ## type: ignore
-        wrapper = lib3mf.get_wrapper()
-        model: lib3mf.Model = wrapper.CreateModel() ## type: ignore
+class ThreeMFModel:
+    def __init__(self, filepath=None):
+        self.wrapper = lib3mf.get_wrapper()
+        self.model: lib3mf.Model = self.wrapper.CreateModel() ## type: ignore
+        if filepath:
+            self.model.QueryReader("3mf").ReadFromFile(filepath)
 
-        try:
-            reader: lib3mf.Reader = model.QueryReader("3mf")
-            reader.ReadFromFile(filepath)
+    def _import_populate_materials(self):
+        color_groups: lib3mf.ColorGroupIterator = self.model.GetColorGroups()
 
-            ## create new blender mesh object
-            bm = bmesh.new()
+        materials = {}
 
-            ## iterate our model meshes
-            # for build_item in model.GetObjects():
-            bi: lib3mf.ObjectIterator = model.GetObjects()
-            meshes = []
+        while color_groups.MoveNext():
+            group: lib3mf.ColorGroup = color_groups.GetCurrentColorGroup()
+            prop_ids = group.GetAllPropertyIDs()
 
-            while bi.MoveNext():
-                obj = bi.GetCurrentObject()
-                if isinstance(obj, lib3mf.MeshObject):
-                    ## we have a mesh object
-                    me = bpy.data.meshes.new("3MF_Mesh")
+            for pid in prop_ids:
+                color = group.GetColor(pid)
 
-                    translation = self._3mf2blender_translation(obj)
+                linear_rgba = lib3mf_color_to_linear(color)
+                mat_name = f"ColorGroup_{group.GetResourceID()}_{pid}"
+                mat = create_principled_material(mat_name, color=linear_rgba, alpha=linear_rgba[3])
+                materials[(group.GetResourceID(), pid)] = mat
 
-                    me.from_pydata(*translation)
-                    meshes.append(me)
-            
-            for mesh in meshes:
-                obj = bpy.data.objects.new("3MF_Object", mesh)
+        return materials
 
-                if context.collection:
-                    context.collection.objects.link(obj)
+    def _import_mesh_objects(self, context, materials: dict[tuple[int, int], Material]):
+        bi: lib3mf.MeshObjectIterator = self.model.GetMeshObjects()
 
-                ## select and make active
-                if bpy.context.view_layer:
-                    bpy.context.view_layer.objects.active = obj
-                obj.select_set(True)
+        while bi.MoveNext():
+            mesh: lib3mf.MeshObject = bi.GetCurrentMeshObject()
+            me = bpy.data.meshes.new(mesh.GetName() or '3mf Mesh')
 
-                self.report({'INFO'}, f"obj = {obj}")
-            return {'FINISHED'}
-        except Exception as e:
-            self.report({'ERROR'}, f"Failed to import 3MF file: {e}")
-            return {'CANCELLED'}
+            verts, _, faces = self._3mf2blender_translation(mesh)
+
+            me.from_pydata(verts, [], faces)
+            # meshes.append(me)
+
+            obj = bpy.data.objects.new(mesh.GetName() or '3mf Object', me)
+
+            if context.collection:
+                context.collection.objects.link(obj)
+
+            # Assign materials to the mesh
+            props = mesh.GetAllTriangleProperties()
+            mat_indices = {}
+            for t_index, t in enumerate(faces):
+                prop = props[t_index]
+                key = (prop.ResourceID, prop.PropertyIDs[0])  # ColorGroup + property
+                mat = materials.get(key)
+                if mat:
+                    # Add material to mesh if not added yet
+                    if mat.name not in obj.data.materials:
+                        obj.data.materials.append(mat)
+                    mat_slot_index = obj.data.materials.find(mat.name)
+                    # Assign polygon’s material index
+                    me.polygons[t_index].material_index = mat_slot_index
+
+            ## select and make active
+            if bpy.context.view_layer:
+                bpy.context.view_layer.objects.active = obj
+            obj.select_set(True)
+
+    def save(self, filepath):
+        writer = self.model.QueryWriter("3mf")
+        writer.WriteToFile(filepath)
+
     def _3mf2blender_translation(self, mesh):
         vertices = mesh.GetVertices()
         triangles = mesh.GetTriangleIndices()
@@ -76,51 +89,83 @@ class Import3MF(bpy.types.Operator, ImportHelper):
 
         return (verts, [], faces)
 
-classes = (Import3MF,)
+class Import3MF(bpy.types.Operator, ImportHelper):
+    """
+        Import a 3mf file
+    """
+    bl_idname = "import_3mf.3mf"
+    bl_label = "Import 3MF (3D Manufacturing Format)"
+    bl_options = {'PRESET', 'UNDO'}
 
-class MF_OT_preferences(bpy.types.Operator):
-    bl_idname = "import_3mf.preferences"
-    bl_label = "Preferences"
-    bl_description = ("Define addon specific preferences")
-    bl_options = {"REGISTER", "INTERNAL"}
+    filename_ext = ".3mf"
+    filter_glob: StringProperty(default="*.3mf", options={'HIDDEN'}, maxlen=255) ## type: ignore
+    
+    def execute(self, context):
+        filepath = self.filepath ## type: ignore
 
+        try:
+            tmf_model = ThreeMFModel(filepath=filepath)
+            materials = tmf_model._import_populate_materials()
+            self.report({'INFO'}, f"materials = {materials}")
+            tmf_model._import_mesh_objects(context, materials)
+
+            return {'FINISHED'}
+        except Exception as e:
+            self.report({'ERROR'}, f"Failed to import 3MF file: {e}")
+            return {'CANCELLED'}
+
+class Export3MF(bpy.types.Operator, ExportHelper):
+    """
+        Export a 3mf file
+    """
+    bl_idname = "export_3mf.3mf"
+    bl_label = "Export 3MF (3D Manufacturing Format)"
+    bl_options = {'PRESET', 'UNDO'}
+
+    filename_ext = ".3mf"
+    filter_glob: StringProperty(default="*.3mf", options={'HIDDEN'}, maxlen=255) ## type: ignore
+    
+    @classmethod
+    def poll(cls, context: Context) -> bool:
+        return False
 
     def execute(self, context):
-        return {"FINISHED"}
+        filepath = self.filepath ## type: ignore
 
+        # try:
+        #     tmf_model = ThreeMFModel(filepath=filepath)
+        #     materials = tmf_model._import_populate_materials()
+        #     self.report({'INFO'}, f"materials = {materials}")
+        #     tmf_model._import_mesh_objects(context, materials)
 
-class MF_preferences(bpy.types.AddonPreferences):
-    bl_idname = __name__
+        #     return {'FINISHED'}
+        # except Exception as e:
+        self.report({'ERROR'}, f"Exporting has not been fully implemented yet!")
+        return {'ERROR'}
 
-    def draw(self, context):
-        layout = self.layout
-        layout.operator(MF_OT_preferences.bl_idname, icon="CONSOLE")
-
-
-preference_classes = (MF_OT_preferences, MF_preferences)
+classes = (Import3MF, Export3MF)
 
 ## Blender Registration
 def menu_func_import(self, context):
     self.layout.operator(Import3MF.bl_idname, text="3D Manufacturing Format (.3mf)")
 
+def menu_func_export(self, context):
+    self.layout.operator(Export3MF.bl_idname, text="3D Manufacturing Format (.3mf)")
+
 def register():
-    ## load our preference classes
-    for cls in preference_classes:
-        bpy.utils.register_class(cls)
-    
     for cls in classes:
         bpy.utils.register_class(cls)
 
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+
 
 def unregister():
-    for cls in preference_classes:
-        bpy.utils.unregister_class(cls)
-
     for cls in classes:
         bpy.utils.unregister_class(cls)
 
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
 
 if __name__ == "__main__":
     register()
---
+++ threemf_format/utils.py
@@ -0,0 +1,45 @@
+import bpy
+
+def create_principled_material(name, color=(1, 1, 1, 1), roughness=0.5, metallic=0.0, alpha=1.0):
+    mat = bpy.data.materials.new(name=name)
+    mat.use_nodes = True
+    mat.use_transparent_shadow = True if alpha < 1.0 else False
+    mat.blend_method = 'BLEND' if alpha < 1.0 else 'OPAQUE'
+    # mat.shadow_method = 'HASHED' if alpha < 1.0 else 'OPAQUE'
+
+    bsdf = mat.node_tree.nodes.get("Principled BSDF")
+    if bsdf:
+        bsdf.inputs['Base Color'].default_value = (color[0], color[1], color[2], 1.0)
+        bsdf.inputs['Alpha'].default_value = alpha
+        bsdf.inputs['Roughness'].default_value = roughness
+        bsdf.inputs['Metallic'].default_value = metallic
+
+    return mat
+
+def create_texture_material(name, image_path, uv_layer_name="UVMap"):
+    mat = bpy.data.materials.new(name)
+    mat.use_nodes = True
+    nt = mat.node_tree
+
+    bsdf = nt.nodes["Principled BSDF"]
+    tex_image = nt.nodes.new("ShaderNodeTexImage")
+    tex_image.image = bpy.data.images.load(image_path)
+
+    uv_map = nt.nodes.new("ShaderNodeUVMap")
+    uv_map.uv_map = uv_layer_name
+
+    nt.links.new(uv_map.outputs["UV"], tex_image.inputs["Vector"])
+    nt.links.new(tex_image.outputs["Color"], bsdf.inputs["Base Color"])
+
+    return mat
+
+def lib3mf_color_to_linear(color):
+    def srgb_to_linear(c):
+        return c / 12.92 if c <= 0.04045 else ((c + 0.055) / 1.055) ** 2.4
+
+    return (
+        srgb_to_linear(color.Red / 255.0),
+        srgb_to_linear(color.Green / 255.0),
+        srgb_to_linear(color.Blue / 255.0),
+        color.Alpha / 255.0
+    )
